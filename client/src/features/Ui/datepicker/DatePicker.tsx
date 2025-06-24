a4c6ea6a86093196cfeaba1e43067e54374586a7
Your Name <you@example.com>
2025-06-24 23:16:04 +0300

    fix/bug DatePicker

--- client/src/features/Ui/datepicker/DatePicker.tsx
+++ client/src/features/Ui/datepicker/DatePicker.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 /* eslint-disable jsx-a11y/no-noninteractive-element-to-interactive-role */
 /* eslint-disable jsx-a11y/click-events-have-key-events */
 import React, { useEffect, useState } from "react";
